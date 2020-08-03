41623bd6b57d4dd6db595d2aae37a093a3229d11
phamkhanhhung <43316031+phamkhanhhung@users.noreply.github.com>
2020-08-03 17:34:01 +0700

    Create app

---
+++ DanhBaDienThoai/HomeVC/HomeVC.swift
@@ -0,0 +1,23 @@
+//
+//  HomeVC.swift
+//  DanhBaDienThoai
+//
+//  Created by Phạm Khánh Hưng on 30/7/20.
+//  Copyright © 2020 Phạm Khánh Hưng. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        print("home page")
+    }
+
+
+    
+    
+
+}
---
+++ DanhBaDienThoai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DanhBaDienThoai
+//
+//  Created by Phạm Khánh Hưng on 30/7/20.
+//  Copyright © 2020 Phạm Khánh Hưng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("vC")
+    }
+
+
+}
+
